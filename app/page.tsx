80d924f4fcfe6c319d5538fea7f390b9115d7c8f
japko <lukasz.jablonski.dev@gmail.com>
2024-12-01 20:25:05 +0100

    add first button and task 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      HOME
+      <Button asChild>
+        <Link href="/task/1">Task 1</Link>
+      </Button>
     </div>
   );
 }
---
+++ app/task/1/page.tsx
@@ -0,0 +1,7 @@
+export default function Task1() {
+  return (
+    <div>
+      TASK1
+    </div>
+  );
+}
