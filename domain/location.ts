7f7506c5a61764f30fa8a542c3c227b6d561c937
japko <lukasz.jablonski.dev@gmail.com>
2024-12-01 21:27:50 +0100

    task 1

---
+++ domain/location.ts
@@ -0,0 +1,36 @@
+const difference = (a, b) => {
+  return Math.abs(a - b)
+}
+
+export class Location {
+  private leftLocations: number[] = [];
+  private rightLocations: number[] = [];
+
+  constructor(private data: string[]) {
+    data.forEach((location, index) => {
+      if (index % 2 === 0) {
+        this.leftLocations.push(+location);
+      } else {
+        this.rightLocations.push(+location);
+      }
+
+    })
+
+    this.leftLocations.sort();
+    this.rightLocations.sort();
+
+  }
+
+  public calculate1(): number {
+    let distance = 0
+    const distanceArr = [];
+    this.leftLocations.forEach((location, index) => {
+      const calculatedDifference = difference(location, this.rightLocations[index])
+      distanceArr.push(calculatedDifference)
+      distance += calculatedDifference
+    })
+
+    return distance;
+  }
+
+}
